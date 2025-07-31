1ce888b678d4377697bf93bb3964d1da4555c1ef
Caio Fonsêca <c4iofonseca@gmail.com>
2025-07-31 14:31:09 +0000

    acho que pode ser parecer mais com uma rede social, acho que pode ter ma

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -10,12 +10,12 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent bg-green-500 text-primary-foreground",
+          "border-transparent bg-primary text-primary-foreground",
         secondary:
-          "border-transparent bg-yellow-500 text-secondary-foreground",
+          "border-transparent bg-secondary text-secondary-foreground",
         destructive:
-          "border-transparent bg-red-500 text-destructive-foreground",
-        outline: "text-foreground bg-gray-300",
+          "border-transparent bg-destructive text-destructive-foreground",
+        outline: "text-foreground",
       },
     },
     defaultVariants: {
